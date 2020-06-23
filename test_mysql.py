95c2f244cf2a0a7c1201452d9ce4f51cda1aacec
Rishabh Goel <rgoel1999@gmail.com>
2020-06-23 22:00:48 +0530

    This commit includes the table creation
    Created a database, Created a table and then inserted a row into the table

--- test_mysql.py
+++ test_mysql.py
@@ -3,9 +3,24 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "127.0.0.1",
     user = "root",
-    password = "Rishabh#"
+    password = "Rishabh#",
+    database = "mydatabase"
 )
 
 mycursor = mydb.cursor()
-
+#Created a database:
 mycursor.execute("CREATE DATABASE mydatabase")
+
+#Added table customers:
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), address VARCHAR(20))")
+
+#Added one row into customers:
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John","Highway 21")
+mycursor.execute(sql, val)
+
+#Used to commit changes to the database:
+mydb.commit()
+
+#print the row count that were inserted:
+print(mycursor.rowcount, "record inserted.")
