034625aa3c7b0ac83c007b70231a5a3bba19df94
Rishabh Goel <rgoel1999@gmail.com>
2020-07-07 00:02:02 +0530

    This is a big commit of all
    This commit includes better UI and also added some more Labels
    in what would be the future tab for 'ADD DATA'.

--- UI.py
+++ UI.py
@@ -21,26 +21,32 @@ mycursor = mydb.cursor()
 #Creating the widgets
 root = Tk()
 root.title("Student Data")
-root.geometry("400x400")
+root.geometry("500x400")
 
-Label(root, text="Student Data").grid(row=0, column=0, columnspan=2)
+frame_menu = Frame(root, padx=5,bd=5, bg="#696969")
+frame_menu.pack(side=LEFT, fill=BOTH)
+Button(frame_menu, text="Add Data", width=10, bg="#FFFFFF").grid(row=0, column=0, padx=2, pady=2)
+Button(frame_menu, text="Modify Data", width=10, bg="#FFFFFF").grid(row=1, column=0, padx=2, pady=2)
+Button(frame_menu, text="View Data", width=10, bg="#FFFFFF").grid(row=2, column=0, padx=2, pady=2)
+
+frame_main = Frame(root, padx=5, bd=5, bg="#EEE8AA")
+frame_main.pack(side=LEFT, fill=BOTH, expand=True)
+
+Label(frame_main, text="Student Data", font=("Pacifico", "30"), bg="#EEE8AA", anchor=CENTER).grid(row=0, column=0, columnspan=8)
 
 #Contains the function needed for the command option in Button
-
+lst = ['First Name', "Middle Name", "Last Name", "Roll No", "Batch", "Specialzation", "State", "City"]
 def retrieve():
-    view_data = Toplevel()
-    view_data.title("Student data")
-    view_data.geometry("300x300")
+    view_data = Frame(frame_main, bg="#EEE8AA")
+    view_data.grid(row=8, column=0, columnspan=4)
     query = "Select * from std_data"
     mycursor.execute(query)
     result = mycursor.fetchall()
-    Label(view_data, text="First Name").grid(row=0, column=0, padx=5, pady=5)
-    Label(view_data, text="Middle Name").grid(row=0, column=1, padx=5, pady=5)
-    Label(view_data, text="Last Name").grid(row=0, column=2, padx=5, pady=5)
-    Label(view_data, text="Roll No").grid(row=0, column=3, padx=5, pady=5)
-    for x in range(1, len(result)+1):
-        for index, y in enumerate(result[x-1]):
-            Label(view_data, text=y).grid(row=x, column=index)
+    for index, name in enumerate(lst):
+        Label(view_data, text=name, bg="#EEE8AA", font=("12")).grid(row=0, column=index, padx=5, pady=5)
+    for x in range(len(result)):
+        for index, y in enumerate(result[x]):
+            Label(view_data, text=y, bg="#EEE8AA", anchor=W, font=("12")).grid(row=x+1, column=index)
 
 def add_to_db():
     val = [
@@ -49,13 +55,16 @@ def add_to_db():
         last_name_entry.get(),
         roll_entry.get()
     ]
-    if not all(val):
+    if not any(val):
+        return 1
+    elif not all(val):
         for index, x in enumerate(val):
             if not x:
                 val[index] = "NONE"
     query = "Insert into std_data values (%s,%s,%s,%s)"
     mycursor.execute(query, val)
     mydb.commit()
+    return 0
     # Label(root, text=mycursor.rowcount).grid(row=6, column=1)
 
 def clean():
@@ -65,35 +74,55 @@ def clean():
     roll_entry.delete(0, END)
 
 #Contains all the text fields
-first_name = Label(root, text="First Name: ")
-first_name_entry = Entry(root)
+first_name = Label(frame_main, text="First Name", width=11, bg="#EEE8AA", anchor=W)
+first_name_entry = Entry(frame_main, width=25)
 
-middle_name = Label(root, text="Middle Name:")
-middle_name_entry = Entry(root)
+middle_name = Label(frame_main, text="Middle Name", width=11, bg="#EEE8AA", anchor=W)
+middle_name_entry = Entry(frame_main, width=25)
 
-last_name = Label(root, text="Last Name: ")
-last_name_entry = Entry(root)
+last_name = Label(frame_main, text="Last Name", width=11, bg="#EEE8AA", anchor=W)
+last_name_entry = Entry(frame_main, width=25)
 
-roll = Label(root, text="Roll No")
-roll_entry = Entry(root)
+roll = Label(frame_main, text="Roll No", width=11, bg="#EEE8AA", anchor=W)
+roll_entry = Entry(frame_main, width=25)
 
-test = Button(root, text="Show data!", command=retrieve)
-clear = Button(root, text="Clear fields", command=clean)
-submit = Button(root, text="Submit", command=add_to_db)
+batch = Label(frame_main, text="Batch", width=11, bg="#EEE8AA", anchor=W)
+batch_entry = Entry(frame_main, width=25)
+
+spec = Label(frame_main, text="Specializaton", width=11, bg="#EEE8AA", anchor=W)
+spec_entry = Entry(frame_main, width=25)
+
+state = Label(frame_main, text="State", width=11, bg="#EEE8AA", anchor=W)
+state_entry = Entry(frame_main, width=25)
+
+city = Label(frame_main, text="City", width=11, bg="#EEE8AA", anchor=W)
+city_entry = Entry(frame_main, width=25)
+
+test = Button(frame_main, text="Show data!", command=retrieve)
+clear = Button(frame_main, text="Clear fields", command=clean)
+submit = Button(frame_main, text="Submit", command=add_to_db)
 
 
 #Placing the widgets using grid()
 first_name.grid(row=1, column=0, sticky=W, padx=5, pady=5)
 first_name_entry.grid(row=1, column=1, sticky=W, padx=5, pady=5)
-middle_name.grid(row=2, column=0, sticky=W, padx=5, pady=5)
-middle_name_entry.grid(row=2, column=1, sticky=W, padx=5, pady=5)
-last_name.grid(row=3, column=0, sticky=W, padx=5, pady=5)
-last_name_entry.grid(row=3, column=1, sticky=W, padx=5, pady=5)
-roll.grid(row=4, column=0, sticky=W, padx=5, pady=5)
-roll_entry.grid(row=4, column=1, sticky=W, padx=5, pady=5)
-submit.grid(row=5, column=0, sticky=W, padx=5, pady=5)
-test.grid(row=5, column=1, sticky=W, padx=5, pady=5)
-clear.grid(row=5, column=2, sticky=W, padx=5, pady=5)
+middle_name.grid(row=1, column=2, sticky=W, padx=5, pady=5)
+middle_name_entry.grid(row=1, column=3, sticky=W, padx=5, pady=5)
+last_name.grid(row=1, column=4, sticky=W, padx=5, pady=5)
+last_name_entry.grid(row=1, column=5, sticky=W, padx=5, pady=5)
+roll.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+roll_entry.grid(row=2, column=1, sticky=W, padx=5, pady=5)
+batch.grid(row=3, column=0, sticky=W, padx=5, pady=5)
+batch_entry.grid(row=3, column=1, sticky=W, padx=5, pady=5)
+spec.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+spec_entry.grid(row=4, column=1, sticky=W, padx=5, pady=5)
+state.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+state_entry.grid(row=5, column=1, sticky=W, padx=5, pady=5)
+city.grid(row=6, column=0, sticky=W, padx=5, pady=5)
+city_entry.grid(row=6, column=1, sticky=W, padx=5, pady=5)
+submit.grid(row=7, column=0, sticky=W, padx=5, pady=5)
+test.grid(row=7, column=1, sticky=W, padx=5, pady=5)
+clear.grid(row=7, column=2, sticky=W, padx=5, pady=5)
 
 
 
