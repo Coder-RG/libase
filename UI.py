2aa1123e123f797114d8e59852d9db1475290674
Rishabh Goel <rgoel1999@gmail.com>
2020-07-06 16:45:58 +0530

    Removed Frames for simplicity, maybe in the future

--- UI.py
+++ UI.py
@@ -7,37 +7,31 @@ mydb = mysql.connector.connect(
     port = "3306",
     user = "root",
     password = "Rishabh#",
-    database = "std_info"
 )
 mycursor = mydb.cursor()
 
 mycursor.execute("Create database if not exists std_info")
+mycursor.execute("Use std_info")
+mycursor.execute("Drop table if exists std_info")
 
 mycursor.execute("Create table if not exists std_data (first_name VARCHAR(30), middle_name VARCHAR(30), last_name VARCHAR(30), roll_no VARCHAR(30))"
 )
 
-# mycursor.execute(
-#     "Insert into std_data (first_name, last_name, roll_no) values('Rishabh','Goel', '17/ICS/073')"
-# )
-# mydb.commit()
-# mycursor.execute("Select * from std_data")
-# result = mycursor.fetchall()
-
+#Creating the widgets
 root = Tk()
 root.title("Student Data")
+root.geometry("400x400")
 
-heading = Label(root, text="Student Data")
-details = Frame(root, padx=10, pady=10)
-testing = Frame(root, padx=10, pady=10)
+Label(root, text="Student Data").grid(row=0, column=0, columnspan=2)
 
-#Contains the function for needed for the command option in Label
+#Contains the function needed for the command option in Button
 
 def retrieve():
     query = "Select * from std_data"
     mycursor.execute(query)
     result = mycursor.fetchall()
     for x in result:
-        Label(testing, text=x).pack(side=TOP)
+        Label(root, text=x).grid(row=6, column=0)
 
 def add_to_db():
     val = (
@@ -52,39 +46,34 @@ def add_to_db():
     # Label(details, text=mycursor.rowcount).pack()
 
 #Contains all the text fields
-first_name = Label(details, text="First Name: ")
-first_name_entry = Entry(details)
+first_name = Label(root, text="First Name: ")
+first_name_entry = Entry(root)
 
-middle_name = Label(details, text="Middle Name:")
-middle_name_entry = Entry(details)
+middle_name = Label(root, text="Middle Name:")
+middle_name_entry = Entry(root)
 
-last_name = Label(details, text="Last Name: ")
-last_name_entry = Entry(details)
+last_name = Label(root, text="Last Name: ")
+last_name_entry = Entry(root)
 
-roll = Label(details, text="Book Title")
-roll_entry = Entry(details)
+roll = Label(root, text="Roll No")
+roll_entry = Entry(root)
 
-test = Button(testing, text="Show data!", command=retrieve)
+test = Button(root, text="Show data!", command=retrieve)
 
-more_options = Button(details, text="Submit", command=add_to_db)
+submit = Button(root, text="Submit", command=add_to_db)
 
-#Frames are shown using pack()
-details.pack()
-testing.pack()
 
 #Objects in details using grid()
-first_name.grid(row=0, column=0)
-first_name_entry.grid(row=0, column=1)
-middle_name.grid(row=1, column=0)
-middle_name_entry.grid(row=1, column=1)
-last_name.grid(row=2, column=0)
-last_name_entry.grid(row=2, column=1)
-roll.grid(row=3, column=0)
-roll_entry.grid(row=3, column=1)
-more_options.grid(row=4, column=0, columnspan=2)
-
-#Objects in testing using pack()
-test.pack()#(row=5, column=1)
+first_name.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+first_name_entry.grid(row=1, column=1, sticky=W, padx=5, pady=5)
+middle_name.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+middle_name_entry.grid(row=2, column=1, sticky=W, padx=5, pady=5)
+last_name.grid(row=3, column=0, sticky=W, padx=5, pady=5)
+last_name_entry.grid(row=3, column=1, sticky=W, padx=5, pady=5)
+roll.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+roll_entry.grid(row=4, column=1, sticky=W, padx=5, pady=5)
+submit.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+test.grid(row=5, column=1, sticky=W, padx=5, pady=5)
 
 
 root.mainloop()
